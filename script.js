dea8279ff76b3112a0b947e49d156658e11dc2df
Varun Gowda MJ <104577546+varungowdamj@users.noreply.github.com>
2024-07-12 21:32:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 function makeid(l) {
-  // write your code here
-}
+            const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+            let result = '';
+            const charactersLength = characters.length;
+            for (let i = 0; i < l; i++) {
+                result += characters.charAt(Math.floor(Math.random() * charactersLength));
+            }
+            return result;
+        }
 
-// Do not change the code below.
-const l = prompt("Enter a number.");
-alert(makeid(l));
+        const l = prompt("Enter a number.");
+        if (isNaN(l) || l <= 0 || l > 1000) {
+            alert("Please enter a valid number between 1 and 1000.");
+        } else {
+            alert(makeid(parseInt(l)));
+        }
